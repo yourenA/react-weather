6d51cf9a897b26ff15aa064c2cdbda90337eb2bc
yourena <2373638339@qq.com>
2017-01-16 10:12:19 +0800

    change the module hot

--- app/weather.js
+++ app/weather.js
@@ -5,10 +5,31 @@ let  React = require('react');
 let  ReactDOM = require('react-dom');
 let Weather=require('./components/weather/index');
 
-ReactDOM.render(
-    <div>
-        <Weather url="http://wthrcdn.etouch.cn/weather_mini?city="/>
-    </div>
-    ,
-    document.getElementById('content')
-);
+let render = () => {
+    ReactDOM.render(
+        <div>
+            <Weather url="http://wthrcdn.etouch.cn/weather_mini?city="/>
+        </div>
+        ,
+        document.getElementById('content')
+    );
+};
+
+if (module.hot) {
+    // Support hot reloading of components
+    // and display an overlay for runtime errors
+    const renderApp = render;
+    render = () => {
+        try {
+            renderApp();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const rerender = () => {
+        setTimeout(render);
+    }
+    module.hot.accept('./components/weather/index', rerender);
+}
+
+render();
